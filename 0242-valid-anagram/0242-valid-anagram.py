55fc18bd17cdd658fb9a6370a6b74061f9db5bfe
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-07-20 11:12:39 +0530

    Time: 11 ms (96.01%), Space: 12.4 MB (85.58%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1 
+        for i in t:
+            if i in d:
+                d[i]-=1
+            else:
+                return False
+        for i in d.values():
+            if i != 0:
+                return False
+        return True        
+                              
+        
