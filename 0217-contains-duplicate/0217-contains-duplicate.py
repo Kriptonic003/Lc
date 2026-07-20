9763545e098f40df5d1547bef9f27106f7263772
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-07-20 21:03:29 +0530

    Time: 34 ms (20.19%), Space: 26 MB (33.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,8 @@
-class Solution(object):
+class Solution:
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        if len(set(nums)) == len(nums):
-            return False
-        return True   
-        
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
