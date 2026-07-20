8d15924246cb6fe3bda07842ca1a20a269f6c2b7
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-07-20 21:04:21 +0530

    Time: 0 ms (100%), Space: 13.1 MB (52.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        d={}
+        d = {}
+
         for i in range(len(nums)):
             if target - nums[i] in d:
-                return [i,d[target - nums[i]]]
-            else:
-                d[nums[i]]=i    
-        
+                return [i, d[target - nums[i]]]
+            d[nums[i]] = i
