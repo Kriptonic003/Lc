8326a2036b8a95aacf081feab19ecf6839d1f558
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-07-20 21:02:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.58%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        maximum = max(candies)
+        ans = []
+        for candy in candies:
+            ans.append(candy + extraCandies >= maximum)
+        return ans
