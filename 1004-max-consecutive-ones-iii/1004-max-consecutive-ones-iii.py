a395f3103bd6457d20177f7997cc81eea467d7b6
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-08-25 11:18:44 +0530

    Time: 90 ms (74.97%), Space: 15.8 MB (76.02%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -5,16 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        ans=0
-        zero=0
         l=0
-        for  r in range(len(nums)):
+        zero=0
+        ans=0
+        for r in range(len(nums)):
             if nums[r]==0:
                 zero+=1
             while zero > k:
                 if nums[l]==0:
                     zero-=1
                 l+=1
-            ans=max(ans,r-l+1)  
-        return ans             
+            ans=max(ans,r-l+1)
+        return ans                
         
