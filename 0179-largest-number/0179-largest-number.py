6cae37862de9ab6e35df71f1fbaaf23f469688a2
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-08-26 09:56:36 +0530

    Time: 9 ms (28.96%), Space: 12.6 MB (12.92%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -12,10 +12,7 @@ class Solution(object):
             else:
                 return 1
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if compare(nums[i], nums[j]) == 1:
-                    nums[i], nums[j] = nums[j], nums[i]
+        nums = sorted(nums, key=cmp_to_key(compare))
         if nums[0] == 0:
             return "0"            
 
