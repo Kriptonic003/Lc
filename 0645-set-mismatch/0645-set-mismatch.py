5a4a6ee7d5e886f5b2a246c7ad159cebd57c4ab2
Kriptonic003 <144616802+Kriptonic003@users.noreply.github.com>
2026-08-27 11:50:05 +0530

    Time: 19 ms (48.91%), Space: 14.1 MB (24.64%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -7,10 +7,17 @@ class Solution(object):
         mx=max(nums)
         mn=min(nums)
         s=set(nums)
-        for i in range(len(nums)):
-            if nums.count(nums[i])>1:
-                dup=nums[i]
+        # for i in range(len(nums)):
+        #     if nums.count(nums[i])>1:
+        #         dup=nums[i]
+        #         break
+        seen = set()
+
+        for n in nums:
+            if n  in seen:
+                dup = n
                 break
+            seen.add(n)
         for i in range(1,mx+1):
             if i not in s:
                 return [dup,i] 
